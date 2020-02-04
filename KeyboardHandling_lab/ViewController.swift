7e5f324583f82212fd1ff32c0963e41f9504e07d
David <lin.david985@gmail.com>
2020-02-04 09:48:21 -0500

    Initial Commit

---
+++ KeyboardHandling_lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeyboardHandling_lab
+//
+//  Created by David Lin on 2/4/20.
+//  Copyright © 2020 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
